e3be0a0f3388a678c7bb9c8605976b0beeb25900
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-11-27 19:53:54 +0300

    Add footer markdown. Remove wraps in header.

--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,11 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { AppHeader } from './app-header/app-header';
 import { AppFooter } from './app-footer/app-footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppHeader, AppFooter],
+  imports: [/*RouterOutlet, */AppHeader, AppFooter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
